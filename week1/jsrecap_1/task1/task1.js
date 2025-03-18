33b16cfb0131704e99fc8de9378e9221340ca6de
Stainamou <margussten@gmail.com>
2025-03-18 13:21:52 +0200

    js tasks

---
+++ week1/jsrecap_1/task1/task1.js
@@ -0,0 +1,8 @@
+let C = parseFloat(prompt("Enter the temperature in Celsius: "));
+
+let K, F
+F = (C * 9/5) + 32
+K = C + 273.15
+
+document.querySelector('#result').innerHTML = "The temperature in Fahrenheit is: " + F + "<br>";
+document.querySelector('#result2').innerHTML = "The temperature in Kelvin is: " + K;
---
+++ week1/jsrecap_1/task2/task2.js
@@ -0,0 +1,7 @@
+let x1 = parseInt(prompt("Enter x1: "));
+let x2 = parseInt(prompt("Enter x2: "));
+let y1 = parseInt(prompt("Enter y1: "))
+let y2 = parseInt(prompt("Enter y2: "));
+
+let distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+document.querySelector("#result").innerHTML = "The distance between two points is: " + distance;
